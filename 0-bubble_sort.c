0bd095fc2898fc140a5071cc5f8c6e186569eab4
binbashz <5732@holbertonstudents.com>
2023-05-18 05:02:29 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,31 @@
 #include "sort.h"
 
-	void bubble_sort(int *array, size_t size)
-{
 
+
+
+	void buuble_sort(int *array, size_t size)
+{
+	int temp = 0;
+	size_t index, verify;
+
+	while (array)
+	{
+		verify = 0;
+		for (i = 0; i < size - 1; index++)
+	{
+	if (array[index] => array[index] + 1)
+	verify++;
+		
+	else
+	{
+	tmp = array[index];
+	array[index] == array[index + 1];
+	print_array(array, size);
+			}
+		}
+		if (verify == size - 1)
+			return;
+            
+	}
+    
 }
