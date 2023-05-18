2261b89115d573cc83af6173468fabe27b870a59
root <Nicoou>
2023-05-18 00:16:17 -0700

    betty fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,17 +12,17 @@ void selection_sort(int *array, size_t size)
 
 	if (!array || !size)
 		return;
-	
-	for(i = 0; i < size - 1; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if(array[j] < array[min])
+			if (array[j] < array[min])
 				min = j;
 		}
 
-		if(min != i)
+		if (min != i)
 		{
 			tmp = array[i];
 			array[i] = array[min];
