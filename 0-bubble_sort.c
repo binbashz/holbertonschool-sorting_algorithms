145fcc899c887570734d25131d6a70eab0cab6c2
binbashz <5732@holbertonstudents.com>
2023-05-18 05:30:47 -0700

    ;

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,11 @@
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order 
+ *
+ * @array: the array to be sorted
+ * @size: number of elements in array
+ */
+
 #include "sort.h"
 
 
@@ -11,21 +19,24 @@
 	while (array)
 	{
 		verify = 0;
-		for (index = 0; index < size - 1; index++)
-	{
+	for (index = 0; index < size - 1; index++)
+
+		{
 	if (array[index] <= array[index + 1])
 	verify++;
-		
+
 	else
 	{
+
 	temp = array[index];
 	array[index] = array[index + 1];
+	array[index + 1] = temp;
 	print_array(array, size);
+
 	}
 }
 		if (verify == size - 1)
 		return;
-            
+
 	}
-    
 }
