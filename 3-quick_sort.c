e6270e99f6d9116ee54f473a8a6f82bbcfb57ad7
root <Nicoou>
2023-05-19 14:33:42 -0700

    fix errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -17,7 +17,7 @@ void quick_sort(int *array, size_t size)
 }
 
 /**
- * _recursion - Recursive function used for the Quick sort algorithm.
+ * recursion - Recursive function used for the Quick sort algorithm.
  *
  * @array: Pointer to the array to be sorted.
  * @size: Size of the array (number of elements).
@@ -25,11 +25,11 @@ void quick_sort(int *array, size_t size)
  * @max: The value that is furthest to the right.
  */
 
-void _recursion(int *array, size_t size, int min, int max)
+void recursion(int *array, size_t size, int min, int max)
 {
 	if (min < max)
 	{
-		int pivot = partition(array, size, min, max);
+		int pivot = part(array, size, min, max);
 
 		recursion(array, size, min, pivot - 1);
 		recursion(array, size, pivot + 1, max);
@@ -37,7 +37,7 @@ void _recursion(int *array, size_t size, int min, int max)
 }
 
 /**
- * _part - Lomuto partition scheme for Quick sort algorithm.
+ * part - Lomuto partition scheme for Quick sort algorithm.
  *
  * @array: Pointer to the array to be partitioned.
  * @size: Size of the array (number of elements).
@@ -47,7 +47,7 @@ void _recursion(int *array, size_t size, int min, int max)
  * Return: The value of the pivot element after partitioning.
  */
 
-int _part(int *array, size_t size, int min, int max)
+int part(int *array, size_t size, int min, int max)
 {
 	int pivot = array[max];
 	int i = min - 1, j;
@@ -57,29 +57,34 @@ int _part(int *array, size_t size, int min, int max)
 		if (array[j] < pivot)
 		{
 			i++;
-			if (i != j)
-				swap(&array[i], &array[j]);
+			swap(array, size, i, j);
 		}
 	}
 
-	if (i + 1 != max)
-		swap(&array[i + 1], &array[max]);
+	swap(array, size, i + 1, max);
+
 	return (i + 1);
 }
 
 /**
- * _swap - Swaps two integer values.
+ * swap - Swaps two integer values.
  *
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
+ * @array: Pointer to the array.
+ * @size: Size of the array (number of elements).
+ * @min: Index of the first element to swap.
+ * @max: Index of the second element to swap.
  */
 
-void _swap(int *a, int *b)
+void swap(int *array, size_t size, int min, int max)
 {
-	if (*a != *b)
+	int tmp;
+
+	if (array[min] != array[max])
 	{
-		int tmp = *a;
-		*a = *b;
-		*b = tmp;
+		tmp = array[min];
+		array[min] = array[max];
+		array[max] = tmp;
+
+		print_array(array, size);
 	}
 }
